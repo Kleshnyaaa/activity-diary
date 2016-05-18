66c9deb55037204e61f44657bdcadd4caa80baae
Kiryl <khodkp@gmail.com>
2016-05-18 23:43:57 +0300

    Base template with 'empty' landing, login and signin pages

---
+++ ActivityDiary.Web/ActivityDiary.Web/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActivityDiary.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
